13d241fe8e33d8b01bc24858d5bbadb39691d6ed
Vincent Daubry <vdaubry@gmail.com>
2023-03-07 16:58:21 +0100

    add mobile launch link

--- frontend/components/LandingHeader.js
+++ frontend/components/LandingHeader.js
@@ -116,7 +116,7 @@ export default function LandingHeader() {
               </div>
               <div className="py-6">
                 <a
-                  href="#"
+                  href="/vesting_app"
                   className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   <button
